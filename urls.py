c6bb8f263260588a87a2c030dc1f5453ee6f07d6
DylanPJackson <frprdjackson@gmail.com>
2021-04-18 15:15:06 -0400

    Upload of django attempt 1

---
+++ urls.py
@@ -0,0 +1,6 @@
+from .my_skill import skill
+from django_ask_sdk.skill_adapter import SkillAdapter
+
+my_skill_view = SkillAdapter.as_view(skill = skill)
+
+urlpatterns = [path('/', my_skill_view, name = 'index']
